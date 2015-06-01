c13b3d0009b372168cc2f9e95c196579d49867d5
Vitaly Shutko <sokal32@gmail.com>
2015-06-01 09:40:40 +0300

    fix bugs with id

--- src/ctail_mongo.erl
+++ src/ctail_mongo.erl
@@ -16,8 +16,12 @@
 
 -define(POOL_NAME, mongo_pool).
 
+%% Custom functions
+
 exec(Fun, Args) ->
   poolboy:transaction(?POOL_NAME, fun (W) -> apply(mongo, Fun, [W]++Args) end).
+
+%% Backend callbacks
 
 init() -> 
   connect(),
@@ -50,7 +54,7 @@ drop_table(Table) ->
   exec(command, [{<<"drop">>, to_binary(Table)}]).
 
 destroy() -> 
-  [ drop_table(Table) || {_, Table} <- dir() ],
+  [ drop_table(Table) || Table <- dir() ],
   ok.
 
 next_id(_Table, _Incr) -> 
@@ -77,6 +81,9 @@ to_binary(Value, ForceList) ->
       end
   end.
 
+make_id({<<ObjectId:12/binary>>}) -> {ObjectId};
+make_id(Term)                     -> to_binary(Term, true).
+
 make_field(Value) ->
   if 
     is_atom(Value) -> 
@@ -95,7 +102,7 @@ make_document(Table, Key, Values) ->
   TableInfo = ctail:table(Table), 
   Document = list_to_document(tl(TableInfo#table.fields), Values),
 
-  list_to_tuple(['_id', to_binary(Key)|Document]).
+  list_to_tuple(['_id', make_id(Key)|Document]).
 
 list_to_document([],             [])             -> [];
 list_to_document([Field|Fields], [Value|Values]) ->
@@ -103,7 +110,10 @@ list_to_document([Field|Fields], [Value|Values]) ->
     undefined -> 
       list_to_document(Fields, Values);
     _ -> 
-      [Field, make_field(Value)|list_to_document(Fields, Values)]
+      case Field of
+        feed_id -> [Field, make_id(Value)|list_to_document(Fields, Values)];
+        _       -> [Field, make_field(Value)|list_to_document(Fields, Values)]
+      end
   end.
 
 persist(Record) ->
@@ -111,7 +121,7 @@ persist(Record) ->
   Key           = element(2, Record), 
   [_, _|Values] = tuple_to_list(Record), 
   Document      = make_document(Table, Key, Values),
-  Selector      = {'_id', to_binary(Key)},
+  Selector      = {'_id', make_id(Key)},
 
   exec(update, [to_binary(Table), Selector, Document, true]).
 
@@ -129,7 +139,6 @@ delete(Table, Key) ->
 
 make_record(Table, Document) ->
   TableInfo = ctail:table(Table), 
-  io:format("TABLE: ~p~n", [ctail:table(Table)]),
   PropList  = document_to_proplist(tuple_to_list(Document)), 
   Values    = [proplists:get_value(Field, PropList) || Field <- TableInfo#table.fields],
 
@@ -142,13 +151,22 @@ decode_field({pid, Pid})                  -> list_to_pid(binary_to_list(Pid));
 decode_field(Value) when is_binary(Value) -> unicode:characters_to_list(Value, utf8);
 decode_field(Value)                       -> Value.
 
-document_to_proplist(Doc)                 -> document_to_proplist(Doc, []).
-document_to_proplist([],             Acc) -> Acc;
-document_to_proplist(['_id', V|Doc], Acc) -> document_to_proplist(Doc, [{id, V}|Acc]);
-document_to_proplist([F,     V|Doc], Acc) -> document_to_proplist(Doc, [{F, decode_field(V)}|Acc]).
+decode_id({<<ObjectId:12/binary>>}) -> 
+  {ObjectId};
+decode_id(List) ->
+  {ok, Tokens, _EndLine} = erl_scan:string(lists:append(binary_to_list(List), ".")),
+  {ok, AbsForm}          = erl_parse:parse_exprs(Tokens),
+  {value, Value, _Bs}    = erl_eval:exprs(AbsForm, erl_eval:new_bindings()),
+  Value.
+
+document_to_proplist(Doc)                   -> document_to_proplist(Doc, []).
+document_to_proplist([],               Acc) -> Acc;
+document_to_proplist(['_id',   V|Doc], Acc) -> document_to_proplist(Doc, [{id, decode_id(V)}|Acc]);
+document_to_proplist([feed_id, V|Doc], Acc) -> document_to_proplist(Doc, [{feed_id, decode_id(V)}|Acc]);
+document_to_proplist([F,       V|Doc], Acc) -> document_to_proplist(Doc, [{F, decode_field(V)}|Acc]).
 
 get(Table, Key) ->
-  Result = exec(find_one, [to_binary(Table), {'_id', to_binary(Key)}]), 
+  Result = exec(find_one, [to_binary(Table), {'_id', make_id(Key)}]), 
   case Result of 
     {} -> 
       {error, not_found}; 
